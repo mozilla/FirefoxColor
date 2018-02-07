b7b5e6f80d3cb4062e7657ed3f7e2456ae9cbdc1
Les Orchard <me@lmorchard.com>
2018-02-07 16:51:10 -0500

    Tweak AppBackground to not set background style if theme colors unavailable

--- src/web/lib/components/AppBackground/index.js
+++ src/web/lib/components/AppBackground/index.js
@@ -5,17 +5,20 @@ import { colorToCSS } from '../../../../lib/utils';
 import './index.scss';
 
 export const AppBackground = ({ theme }) => {
-  const colors = {};
-  if (theme) {
+  const style = {};
+
+  // HACK: theme should not be undefined, but somehow it can be - track that down
+  if (theme && theme.colors) {
+    const colors = {};
     Object.keys(theme.colors).forEach(key => {
       colors[key] = colorToCSS(theme.colors[key]);
     });
+    style.background =
+      `linear-gradient(${colors.toolbar} 32.5%, ${colors.accentcolor} 78.1%)`;
   }
 
-  const bgGradient = `linear-gradient(${colors.toolbar} 32.5%, ${colors.accentcolor} 78.1%)`;
-
   return (
-    <div className="app-background" style={{ background: bgGradient }}>
+    <div className="app-background" style={style}>
       <div className="app-background__texture-1"/>
       <div className="app-background__texture-2"/>
     </div>
