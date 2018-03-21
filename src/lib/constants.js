aa3d7ec8e6f9d8a0e0fcb25ab615400d4be13954
John Gruen <john.gruen@gmail.com>
2018-03-21 16:37:36 -0400

    finalize loader

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -1,8 +1,10 @@
 export const CHANNEL_NAME = "Themer";
 
-export const DOWNLOAD_FIREFOX_URL = "https://www.mozilla.org/firefox/new/?utm_campaign=themer-acquisition&utm_medium=referral&utm_source=themer.firefox.com";
+export const DOWNLOAD_FIREFOX_URL =
+  "https://www.mozilla.org/firefox/new/?utm_campaign=themer-acquisition&utm_medium=referral&utm_source=themer.firefox.com";
 
-export const surveyUrl = "https://qsurvey.mozilla.com/s3/Test-Pilot-Themer-Feedback";
+export const surveyUrl =
+  "https://qsurvey.mozilla.com/s3/Test-Pilot-Themer-Feedback";
 
 export const colorLabels = {
   toolbar: "Toolbar Color",
@@ -13,9 +15,28 @@ export const colorLabels = {
   toolbar_field_text: "Search Text"
 };
 
-export const colorsWithAlpha = [
-  "toolbar",
-  "toolbar_field"
-];
+export const colorsWithAlpha = ["toolbar", "toolbar_field"];
 
 export const ESC = 27;
+
+export const loaderQuotes = [
+  {
+    quote:
+      "Mere color, unspoiled by meaning, and unallied with definite form, can speak to the soul in a thousand different ways.",
+    attribution: "Oscar Wilde"
+  },
+  {
+    quote:
+      "The purest and most thoughtful minds are those which love color the most.",
+    attribution: "John Ruskin"
+  },
+  {
+    quote:
+      "I found I could say things with color and shapes that I couldn't say any other way — things I had no words for.",
+    attribution: "Georgia O'Keeffe"
+  },
+  {
+    quote: "Colours and colours and colours of colours",
+    attribution: "Hot Chip"
+  }
+];
--- src/web/lib/components/AppLoadingIndicator/index.js
+++ src/web/lib/components/AppLoadingIndicator/index.js
@@ -1,10 +1,18 @@
 import React from "react";
 import "./index.scss";
+import { loaderQuotes } from "../../../../lib/constants";
 
-export const AppLoadingIndicator = () => (
-  <div className="app-loading-indicator">
-    <p>𝕽</p>
-  </div>
-);
+export const AppLoadingIndicator = () => {
+  const i = Math.floor(Math.random() * loaderQuotes.length);
+  return (
+    <div className="app-loading-indicator">
+      <div className="app-loading-indicator__spinner" />
+      <q className="app-loading-indicator__quote">{loaderQuotes[i].quote}</q>
+      <p className="app-loading-indicator__attribution">
+        &#8211;{loaderQuotes[i].attribution}
+      </p>
+    </div>
+  );
+};
 
 export default AppLoadingIndicator;
