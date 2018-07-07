f44352538df736373e08b5c26aa89a1919556d8b
Andy Tran <andy8tran@gmail.com>
2018-07-06 22:56:24 -0400

    Fixes  #366: Randomize presets on first load

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -3,3 +3,12 @@ export const DEBUG = process.env.NODE_ENV === "development";
 export const makeLog = context => (...args) =>
   // eslint-disable-next-line no-console
   DEBUG && console.log(`[FirefoxColor ${context}]`, ...args);
+
+export const shuffle = array => {
+  const arrayCopy = array.slice();
+  for (let i = arrayCopy.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [arrayCopy[i], arrayCopy[j]] = [arrayCopy[j], arrayCopy[i]];
+  }
+  return arrayCopy;
+};
