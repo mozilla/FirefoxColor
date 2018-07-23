caffb93684e19a5a27985e9edac9e6269af9ddad
Les Orchard <me@lmorchard.com>
2018-07-23 20:58:15 +0000

    PROGRESS get drag and drop working

--- src/web/lib/components/ThemeCustomBackgroundPicker/index.js
+++ src/web/lib/components/ThemeCustomBackgroundPicker/index.js
@@ -1,19 +1,11 @@
 import React from "react";
+import {SortableContainer, SortableElement} from "react-sortable-hoc";
 import {
   CUSTOM_BACKGROUND_MAXIMUM_SIZE,
   CUSTOM_BACKGROUND_ALLOWED_TYPES
 } from "../../../../lib/constants";
 
-export const ThemeCustomBackgroundPicker = props => (
-  <form className="custom-background" onSubmit={e => e.preventDefault()}>
-    <label htmlFor="customBackground">
-      <h2>Custom background image</h2>
-    </label>
-    <ThemeCustomBackgroundSelector {...{ ...props, index: 0 }} />
-    <ThemeCustomBackgroundSelector {...{ ...props, index: 1 }} />
-    <ThemeCustomBackgroundSelector {...{ ...props, index: 2 }} />
-  </form>
-);
+import "./index.scss";
 
 class ThemeCustomBackgroundSelector extends React.Component {
   constructor(props) {
@@ -25,25 +17,19 @@ class ThemeCustomBackgroundSelector extends React.Component {
   }
 
   render() {
-    const {
-      index,
-      themeCustomBackgrounds,
-      themeHasCustomBackgrounds
-    } = this.props;
+    const { item } = this.props;
     const {
       handleClearBackground,
       handleFileChoice,
       handleAlignmentChange,
-      handleTilingChange,
-      handleMoveUp,
-      handleMoveDown
+      handleTilingChange
     } = this;
 
-    const { url, tiling, alignment } = themeCustomBackgrounds[index] || {};
+    const { url, tiling, alignment } = item;
     const { customBackgroundTooLarge, customBackgroundWrongType } = this.state;
 
     return (
-      <div>
+      <li>
         {customBackgroundTooLarge && (
           <p className="error">
             Custom background too large, please choose another.
@@ -60,84 +46,69 @@ class ThemeCustomBackgroundSelector extends React.Component {
 
         <input type="file" id="customBackground" onChange={handleFileChoice} />
 
-        {themeHasCustomBackgrounds && (
-          <div>
-            <button onClick={handleMoveUp}>Up</button>
-            <button onClick={handleMoveDown}>Down</button>
-            <input
-              type="button"
-              id="clearBackground"
-              defaultValue="Clear Background"
-              onClick={handleClearBackground}
-            />
-            <select
-              id="alignment"
-              value={alignment}
-              onChange={handleAlignmentChange}
-            >
-              <option value="">none</option>
-              <option value="bottom">bottom</option>
-              <option value="center">center</option>
-              <option value="left">left</option>
-              <option value="right">right</option>
-              <option value="top">top</option>
-              <option value="center bottom">center bottom</option>
-              <option value="center center">center center</option>
-              <option value="center top">center top</option>
-              <option value="left bottom">left bottom</option>
-              <option value="left center">left center</option>
-              <option value="left top">left top</option>
-              <option value="right bottom">right bottom</option>
-              <option value="right center">right center</option>
-              <option value="right top">right top</option>
-            </select>
-            <select id="tiling" value={tiling} onChange={handleTilingChange}>
-              <option value="">none</option>
-              <option value="no-repeat">no-repeat</option>
-              <option value="repeat">repeat</option>
-              <option value="repeat-x">repeat-x</option>
-              <option value="repeat-y">repeat-y</option>
-            </select>
-          </div>
-        )}
-      </div>
+        <div>
+          <input
+            type="button"
+            id="clearBackground"
+            defaultValue="Clear Background"
+            onClick={handleClearBackground}
+          />
+          <select
+            id="alignment"
+            value={alignment}
+            onChange={handleAlignmentChange}
+          >
+            <option value="">none</option>
+            <option value="bottom">bottom</option>
+            <option value="center">center</option>
+            <option value="left">left</option>
+            <option value="right">right</option>
+            <option value="top">top</option>
+            <option value="center bottom">center bottom</option>
+            <option value="center center">center center</option>
+            <option value="center top">center top</option>
+            <option value="left bottom">left bottom</option>
+            <option value="left center">left center</option>
+            <option value="left top">left top</option>
+            <option value="right bottom">right bottom</option>
+            <option value="right center">right center</option>
+            <option value="right top">right top</option>
+          </select>
+          <select id="tiling" value={tiling} onChange={handleTilingChange}>
+            <option value="">none</option>
+            <option value="no-repeat">no-repeat</option>
+            <option value="repeat">repeat</option>
+            <option value="repeat-x">repeat-x</option>
+            <option value="repeat-y">repeat-y</option>
+          </select>
+        </div>
+      </li>
     );
   }
 
   handleClearBackground = (ev) => {
-    const { index, clearCustomBackground } = this.props;
-    clearCustomBackground({ index });
+    const { clearCustomBackground } = this.props;
+    clearCustomBackground();
     ev.preventDefault();
   }
 
-  handleMove = (newIndex) => {
-    const { index: oldIndex, moveCustomBackground, themeCustomBackgrounds } = this.props;
-    if (newIndex < 0) { return; }
-    if (newIndex > themeCustomBackgrounds.length) { return; }
-    moveCustomBackground({ oldIndex, newIndex });
-  }
-
-  handleMoveUp = () => this.handleMove(this.props.index - 1)
-
-  handleMoveDown = () => this.handleMove(this.props.index + 1)
-
   handleAlignmentChange = (ev) => {
-    const { index, themeCustomBackgrounds, setCustomBackground } = this.props;
-    const { url, tiling } = themeCustomBackgrounds[index] || {};
+    const { item, setCustomBackground } = this.props;
+    const { url, tiling } = item;
     const alignment = ev.target.value;
-    setCustomBackground({ index, url, tiling, alignment });
+    setCustomBackground({ url, tiling, alignment });
   }
 
   handleTilingChange = (ev) => {
-    const { index, themeCustomBackgrounds, setCustomBackground } = this.props;
-    const { url, alignment } = themeCustomBackgrounds[index] || {};
+    const { item, setCustomBackground } = this.props;
+    const { url, alignment } = item;
     const tiling = ev.target.value;
-    setCustomBackground({ index, url, tiling, alignment });
+    setCustomBackground({ url, tiling, alignment });
   }
 
   handleFileChoice = (ev) => {
-    const { index, themeCustomBackgrounds, setCustomBackground } = this.props;
-    const { tiling, alignment } = themeCustomBackgrounds[index] || {};
+    const { item, setCustomBackground } = this.props;
+    const { tiling, alignment } = item;
 
     const file = ev.target.files[0];
 
@@ -159,10 +130,60 @@ class ThemeCustomBackgroundSelector extends React.Component {
     const reader = new FileReader();
     reader.onload = ev => {
       const url = ev.target.result;
-      setCustomBackground({ index, url, tiling, alignment });
+      setCustomBackground({ url, tiling, alignment });
     };
 
     reader.readAsDataURL(file);
+  }
+}
+
+const SortableThemeCustomBackgroundSelector = SortableElement(ThemeCustomBackgroundSelector);
+
+const BackgroundList = SortableContainer(props => {
+  const {
+    clearCustomBackground,
+    setCustomBackground
+  } = props;
+  return (
+    <ul>
+      {props.themeCustomBackgrounds.map((item, index) => (
+        <SortableThemeCustomBackgroundSelector key={index} {...{
+          item,
+          index,
+          clearCustomBackground: (args = {}) => clearCustomBackground({ index, ...args }),
+          setCustomBackground: (args = {}) => setCustomBackground({ index, ...args })
+        }} />
+      ))}
+    </ul>
+  );
+});
+
+export class ThemeCustomBackgroundPicker extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  handleAdd = () => {
+    const { themeCustomBackgrounds, setCustomBackground } = this.props;
+    const index = themeCustomBackgrounds.length;
+    setCustomBackground({ index, url: "", tiling: "", alignment: "" });
+  }
+
+  handleMove = ({ oldIndex, newIndex }) => {
+    const { moveCustomBackground } = this.props;
+    moveCustomBackground({ oldIndex, newIndex });
+  }
+
+  render() {
+    return (
+      <form className="custom-background" onSubmit={e => e.preventDefault()}>
+        <label htmlFor="customBackground">
+          <h2>Custom background image</h2>
+        </label>
+        <button onClick={this.handleAdd}>Add</button>
+        <BackgroundList {...this.props} helperClass="dragHelper" onSortEnd={this.handleMove} />
+      </form>
+    );
   }
 }
 
