38566fe0ee3c6055c642dbaab65dd0c9aae55082
Rebecca <rebjmullin@gmail.com>
2019-09-11 21:30:31 -0400

    updating size info/adding icon-wip

--- src/web/lib/components/StorageSpaceInformation/index.js
+++ src/web/lib/components/StorageSpaceInformation/index.js
@@ -1,41 +1,53 @@
 import React from "react";
 import { connect } from "react-redux";
 import { compose } from "redux";
+import StorageIcon from "./StorageIcon";
+
 import "./index.scss";
 
-export const STORAGE_ERROR_MESSAGE =
-  "Sorry! You cannot add this image/theme as it will put you over your storage capacity. Please delete some saved images/themes to make space.";
-
+export const STORAGE_ERROR_MESSAGE = `<span>Sorry! This cannot be added because you are over your storage limit. <br/>
+  Please delete some images or themes to make space.</span>`;
 export const STORAGE_WARNING_MESSAGE =
   "You have almost reached your storage limit! You may need to delete some saved images/themes to make space.";
 
 export const STORAGE_ERROR_MESSAGE_DURATION = 5000;
 
 export const localStorageSpace = () => {
-  let data = "";
-  for (let key in window.localStorage) {
-    if (window.localStorage.hasOwnProperty(key)) {
-      data += window.localStorage[key];
-    }
+  let size = 0;
+  for (let [key, value] of Object.entries(localStorage)) {
+    size += key.length + value.length;
   }
 
-  return data
-    ? parseFloat(((data.length * 16) / (8 * 1024) / 1000).toFixed(3))
+  let test1 = size
+    ? parseFloat(((size.length * 16) / (8 * 1024) / 1000).toFixed(3))
     : 0;
+
+  console.log("REBB test1", test1);
+
+  const sizeMB = size / 1000000;
+  return parseFloat(sizeMB.toFixed(3));
 };
 
 export const StorageSpaceInformationComponent = props => {
   return (
     <div className="storage-space-information">
-      {props.usedStorage}MB out of 10MB
-      {props.usedStorage > 9 && !props.storageErrorMessage && (
-        <span className="storage-space-information-warning">
-          {STORAGE_WARNING_MESSAGE}
-        </span>
+      <div className="storage-space-information-content"><span>{props.usedStorage}MB out of 5.242MB</span> <StorageIcon /></div>
+      {props.usedStorage > 4.75 && !props.storageErrorMessage && (
+        <div className="storage-space-information-warning">
+          <span>
+            You have almost reached your storage limit! <br /> You may need to
+            delete some images or themes to make space.
+          </span>
+        </div>
       )}
       {props.storageErrorMessage && (
         <strong className="storage-space-information-error">
-          {props.storageErrorMessage}
+          <div
+            className="storage-space-information-warning"
+            dangerouslySetInnerHTML={{
+              __html: props.storageErrorMessage
+            }}
+          />
         </strong>
       )}
     </div>
