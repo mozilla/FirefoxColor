8b4d61255ffbf6a36b083a0d5dfaa4b4eba8f52c
Rebecca <rebjmullin@gmail.com>
2019-12-16 23:36:17 -0500

    fix for deleting themes/local storage

--- src/web/lib/components/SavedThemeSelector/index.js
+++ src/web/lib/components/SavedThemeSelector/index.js
@@ -17,8 +17,33 @@ export const SavedThemeSelector = ({
   );
 
   const deleteTheme = (key) => {
+    const themeBeingDeleted = themeStorage.get(key);
     themeStorage.delete(key);
-    clearCustomBackground({ index: 0 });
+    const { images } = themeBeingDeleted.theme;
+
+    // Remove current images from the display and local storage.
+    if (images && images.custom_backgrounds && images.custom_backgrounds.length) {
+      const entries = Object.entries(savedThemes);
+      let themes = entries.filter(([entry]) => entry !== key);
+      const values = Object.values(themes);
+
+      const savedThemeImages = values.map(([_, item]) => item.theme.images.custom_backgrounds).reduce((acc, item) => {
+        (item || []).forEach((bg, i) => {
+          acc.push(bg.name);
+        });
+        return acc;
+      }, []);
+
+
+      const { custom_backgrounds: customImages } = images;
+      let currentImages = customImages.map(({ name }) => name);
+
+      for (let index = currentImages.length - 1; index >= 0; index--) {
+        if (!savedThemeImages.includes(currentImages[index])) {
+          clearCustomBackground({ index });
+        }
+      }
+    }
   };
 
   return (
