1ddc6dd22dedb13811c9ebf76d68b40ee12400a1
Rebecca <rebjmullin@gmail.com>
2020-04-05 17:42:21 -0400

    feedback

--- gen-environment.sh
+++ gen-environment.sh
@@ -12,4 +12,12 @@ elif [ "${CIRCLE_BRANCH}" == "production" ]
 then
   SITE_URL="https://color.firefox.com/"
   ADDON_URL="https://addons.mozilla.org/firefox/addon/firefox-color/"
+else
+  echo "Unknown branch: ${CIRCLE_BRANCH}"
 fi
+
+# This will be appended to $BASH_ENV in .circleci/config.yml
+cat <<ENV_HERE
+export SITE_URL="${SITE_URL}"
+export ADDON_URL="${ADDON_URL}"
+ENV_HERE
